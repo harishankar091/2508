7af529b7c2f4f28716e750f67ebe8a450d25b868
harishankar091 <hari.kommina@sasi.in>
2025-06-06 23:02:20 +0530

    mahindracar

---
+++ mahindracar.java
@@ -0,0 +1,23 @@
+public class mahindracar {
+    public static void main (String[] args) {
+        //coments which are ignored by the compiler
+        //define varaibles
+        byte seatingCapacity = 4;
+        short engineCapacity =  2184;
+        int price = 1700000;
+        float safetyRating = 4f;
+        float reviewRating = 4.5f;
+        double groundClearance = 226.0;
+        String brand = "Mahindra";
+        String description = "MAHINDRA THAR 4X4 LX DIESEL AT HARD TOP";
+        System.out.println(brand);
+        System.out.println(description);
+        System.out.println(seatingCapacity);
+        System.out.println(engineCapacity);
+        System.out.println(price);
+        System.out.println(safetyRating);
+        System.out.println(reviewRating);
+        System.out.println(groundClearance);
+    }
+    
+}
