a67abe98e3ca3a44afb5bd537e34127a6d9eaf84
harishankar091 <hari.kommina@sasi.in>
2025-06-06 22:03:27 +0530

    update myntraproduct

---
+++ productdetail.java
@@ -0,0 +1,24 @@
+public class productdetail {
+    public static void main(String[] args) {
+        // comments which are ignored by the compiler
+        //define variables
+        byte discount = 81;
+        short sellingPrice = 750;
+        int originalPrice = 3949;
+        float rating = 4.6f;
+        int size1 = 39;
+        int size2 = 40;
+        int size3 = 41;
+        int size4 = 42;
+        String Brand = "HIGHLANDER";
+        String description = "Men Comfort Relaxed Fit Textured Spread Collar Casual Shirt";
+        System.out.println("HIGHLANDER");
+        System.out.println("description");
+        System.out.println("discount");
+        System.out.println("sellingPrice");
+        System.out.println("originalPrice");
+        System.out.println("rating");
+        System.out.println("size1")
+
+    }
+}
