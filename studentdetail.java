4c7eb7244aa7e5cf8565dd8491eefa059697ba55
harishankar091 <hari.kommina@sasi.in>
2025-06-10 13:07:43 +0530

    JOINING THE COURSE A STUDENT

---
+++ studentdetail.java
@@ -0,0 +1,24 @@
+public class studentdetail {
+    public static void main(String[] args) {
+        // courses 
+        String[] courses = {"Java", "Python", "C++", "JavaScript", "Sql"};
+        // student details
+        int studentid = 101;
+        String studentname = "Hari";
+        String Studentemail = "harishankarkommina4.gmail.com";
+        String studentaddress = "Nidadavole, Andhra Pradesh";
+        char studentgender = 'M';
+        int phonenumber = 741661849;
+        // print student details
+        System.out.println("studentid is " + studentid);
+        System.out.println("studentname is " + studentname);
+        System.out.println("Studentemail is " + Studentemail);
+        System.out.println( "studentaddress is" + studentaddress);
+        System.out.println("studentgender is " + studentgender);
+        System.out.println("phonenumber is " + phonenumber);
+        // print courses
+        System.out.println("JAVA");
+
+
+    }
+}
