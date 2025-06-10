c3cde243c3e7826cec39fa6e9a770873ad62f0cb
harishankar091 <hari.kommina@sasi.in>
2025-06-10 13:40:45 +0530

    application for college adimission test

---
+++ application.java
@@ -0,0 +1,42 @@
+public class application {
+    public static void main (String[] args) {
+        // student details
+        String firstName = "kommina";
+        String middleName = "hari";
+        String lastname = "shankar";
+        int dateofbirth = 2004;
+        String birthplace = "nidadavole";
+        int phonenumber = 741661849;
+        int yearofpassing = 2020;
+        char studentgender = 'M';
+        String address = "nidadavole";
+        String joingcourse = "B.tech";
+        // parent details
+        String Fathername = "kommina vara prasad";
+        String occupation = "farmer";
+        int fathernumber = 949071849;
+        String Mothername = "kommina venkata lakshmi";
+        String motheroccupation = "housewife";
+        int mothernumber = 967666169;
+        String fatherandmotheraddress = "nidadavole";
+        //print student details
+        System.out.println("firstName is " + firstName);
+        System.out.println("middleName is " + middleName);
+        System.out.println("lastname is " + lastname);
+        System.out.println("dateofbirth is " + dateofbirth);
+        System.out.println("birthplace is " + birthplace);
+        System.out.println("phonenumber is " + phonenumber);
+        System.out.println("yearofpassing is "+ yearofpassing);
+        System.out.println("studentgenderis " + studentgender);
+        System.out.println("address is " + address);
+        System.out.println("joingcourse is " + joingcourse);
+        //print parent details
+        System.out.println("Fathername is " +Fathername);
+        System.out.println("occupation is " + occupation);
+        System.out.println("fathernumber is " + fathernumber);
+        System.out.println("Mothername is " + Mothername);
+        System.out.println("motheroccupation is "+ motheroccupation);
+        System.out.println("mothernumber is " + mothernumber);
+        System.out.println("fatherandmotheraddress is " + fatherandmotheraddress);
+    }
+}
