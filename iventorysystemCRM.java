f71ae5c6a20c9c9689e500a1ff5d1ff057817946
harishankar091 <hari.kommina@sasi.in>
2025-06-12 11:50:52 +0530

    inventory system using operation for CRM

---
+++ iventorysystemCRM.java
@@ -0,0 +1,19 @@
+public class iventorysystemCRM {
+    public static void main(String[] args) {
+        String productname = "iphone 16";
+        int productquatity = 10;
+        double unitprice = 9999.99;
+        boolean reorderthreshold = true;
+        // calcualtion using operators
+        double totalvalueinstock = productquatity * unitprice;
+        boolean productquatityisbelowreorderthreshold = productquatity > 6;
+        boolean issufficientandproductismarkedforsale = productquatity > 6 && reorderthreshold;
+        productquatity--; // decrement product quatity after sale
+        //print results
+        System.out.println("productname is " + productname);
+        System.out.println("totalvalueinstock is " + totalvalueinstock);
+        System.out.println("productquatityisbelowreorderthreshold is " + productquatityisbelowreorderthreshold);
+        System.out.println("issufficientandproductismarkedforsale is " + issufficientandproductismarkedforsale);
+        System.out.println("productquatity is " + productquatity);
+    }
+}
