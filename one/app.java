de5a6e57cf94f27757a61870f685758a67589c57
harishankar091 <hari.kommina@sasi.in>
2025-07-06 19:59:09 +0530

    Static and non static

---
+++ one/app.java
@@ -0,0 +1,13 @@
+package one;
+
+public class app {
+    public static void main(String[] args) {
+        //  static variable
+    int x = 10;
+    int y = 20;
+    int sum = x + y;
+    System.out.println(sum);
+
+    }
+    
+}
