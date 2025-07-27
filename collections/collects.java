a501c8156e0932052f9085ff402231a96d45c54f
harishankar091 <hari.kommina@sasi.in>
2025-07-27 12:10:34 +0530

    COLLECTIONS

---
+++ collections/collects.java
@@ -0,0 +1,27 @@
+package collections;
+
+import java.util.LinkedList;
+
+public class collects {
+    public static void main(String[] args) {
+        // Create a collection
+        LinkedList<String> collection = new LinkedList<>();
+
+        // Add elements to the collection
+        collection.add("Java");
+        collection.add("python");
+        collection.add("C++");
+        collection.add("JavaScript");
+
+        // remove an element
+        collection.remove("C++");
+
+        System.out.println("Collection after removals: " + collection);
+
+        // Iterate through the collection
+        for (String lang : collection) {
+            System.out.println(lang);
+        }
+    }
+
+}
